9541e36802de5246dc98328c1ecb70b158553e3a
Ramanpreet Chand <rchand595@gmail.com>
2017-03-04 18:56:51 -0500

    loaded models

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const flash = require('connect-flash');
 const expressSession = require('express-session');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-// const models = require('../models');
+const models = require('../models');
 
 const app = express();
 app.use(methodOverride('_method'));
