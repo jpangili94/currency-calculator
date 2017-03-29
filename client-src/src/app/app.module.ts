4021e6307e7ba4d0dae96509c442f4dc1cf5af28
Ramanpreet Chand <rchand595@gmail.com>
2017-03-29 18:53:46 -0400

    connected Routes and updated front-end with template-html

--- client-src/src/app/app.module.ts
+++ client-src/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'; //bring in router module
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,13 +12,20 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DashboardComponent,
     SidenavComponent,
-    ProfileComponent,
     ProfileComponent,
     RegisterComponent,
     LoginComponent
